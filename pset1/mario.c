e6af9e1b4761a335fe6179669b9b3ffa07625812
Ibrahim Kamara <ibz.kamara@gmail.com>
2017-01-15 15:41:23 -0500

    Create mario.c

---
+++ pset1/mario.c
@@ -0,0 +1,42 @@
+
+
+#include <stdio.h>
+#include <cs50.h>
+
+
+int main(void)
+
+{
+    
+    int height; 
+    do 
+    {
+        printf("Please give me a height: ");
+        height = get_int();
+        
+    } while(height < 0 || height > 23); 
+    
+    
+    for(int i = 0; i < height; i++) 
+    {
+        
+        for(int j = 0; j < height-i-1; j++)
+        {
+            printf(" ");
+            
+            
+        }
+        
+            for(int k = 0; k < i+2; k++)
+            {
+                printf("#");
+            }
+        
+            printf("\n");
+    }
+    
+    
+    
+    
+    
+}
